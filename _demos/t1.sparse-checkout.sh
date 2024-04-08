99362bdc108236a7e0d342d868777beef7b47d6f
Allen Guo <guosonglin001@gmail.com>
2024-04-08 22:58:13 +0800

    wip

--- _demos/t1.sparse-checkout.sh
+++ _demos/t1.sparse-checkout.sh
@@ -3,7 +3,9 @@ git clone -n --depth=1 --filter=tree:0 -b allen/sycl git@github.com:gglin001/llv
 
 pushd llvm-dpcpp
 git sparse-checkout set --no-cone \
-  /buildbot /clang /cmake /devops /libclc /llvm /llvm-spirv /opencl /sycl /sycl-fusion /xpti /xptifw
+  /buildbot /clang /cmake /devops /libclc /llvm /llvm-spirv /opencl /sycl /sycl-fusion /xpti /xptifw \
+  /_demos /libdevice
+
 git sparse-checkout list
 git checkout
 popd

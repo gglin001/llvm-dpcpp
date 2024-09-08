0cfa760bb478648f56b71f50c7e8c23b9dcfdd86
Allen Guo <guosonglin001@gmail.com>
2024-09-08 13:58:19 +0800

    wip

--- _demos/t0.build.sh
+++ _demos/t0.build.sh
@@ -1,3 +1,24 @@
-python buildbot/configure.py --use-lld --native_cpu
+###############################################################################
+
+python buildbot/configure.py --use-lld --native_cpu \
+  --cmake_opt "FETCHCONTENT_BASE_DIR=$PWD/_demos/_deps_osx"
 
 python buildbot/compile.py
+
+###############################################################################
+
+python buildbot/configure.py --use-lld --native_cpu \
+  --cmake_opt "FETCHCONTENT_BASE_DIR=$PWD/_demos/_deps_linux"
+
+python buildbot/compile.py
+
+###############################################################################
+
+python buildbot/configure.py \
+  --use-lld \
+  --host-target "host;RISCV;AArch64" \
+  --native_cpu
+
+python buildbot/compile.py
+
+###############################################################################

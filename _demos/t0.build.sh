361acb215786339e389191402200ac9ade3cb96b
Allen Guo <guosonglin001@gmail.com>
2024-09-08 15:06:56 +0000

    wip

--- _demos/t0.build.sh
+++ _demos/t0.build.sh
@@ -6,6 +6,8 @@ args=(
   --native_cpu
   --host-target="host;RISCV;AArch64"
   --cmake-opt="-DFETCHCONTENT_BASE_DIR=$PWD/_demos/_deps_osx"
+  --cmake-opt="-DLLVM_INCLUDE_TESTS=OFF"
+  --cmake-opt="-DSYCL_INCLUDE_TESTS=OFF"
 )
 python "${args[@]}"
 
@@ -19,6 +21,8 @@ args=(
   --native_cpu
   --host-target="host;RISCV;AArch64"
   --cmake-opt="-DFETCHCONTENT_BASE_DIR=$PWD/_demos/_deps_linux"
+  --cmake-opt="-DLLVM_INCLUDE_TESTS=OFF"
+  --cmake-opt="-DSYCL_INCLUDE_TESTS=OFF"
 )
 python "${args[@]}"
 

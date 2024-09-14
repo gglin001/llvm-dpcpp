bb6643ed4529af5881cfad2a4595cd0812316636
Allen Guo <guosonglin001@gmail.com>
2024-09-14 09:13:20 +0000

    wip

--- _demos/t0.build.sh
+++ _demos/t0.build.sh
@@ -1,5 +1,12 @@
 ###############################################################################
 
+# build all
+cmake --build build --target all
+
+###############################################################################
+
+# OSX
+
 args=(
   buildbot/configure.py
   --use-lld
@@ -15,6 +22,8 @@ python buildbot/compile.py
 
 ###############################################################################
 
+# NATIVECPU_USE_OCK=ON
+
 args=(
   buildbot/configure.py
   --use-lld
@@ -23,12 +32,15 @@ args=(
   --cmake-opt="-DFETCHCONTENT_BASE_DIR=$PWD/_demos/_deps_linux"
   --cmake-opt="-DLLVM_INCLUDE_TESTS=OFF"
   --cmake-opt="-DSYCL_INCLUDE_TESTS=OFF"
+  --cmake-opt="-DNATIVECPU_USE_OCK=ON"
 )
 python "${args[@]}"
 
 python buildbot/compile.py
 
 ###############################################################################
+
+# NATIVECPU_USE_OCK=OFF
 
 args=(
   buildbot/configure.py
@@ -39,6 +51,25 @@ args=(
   --cmake-opt="-DLLVM_INCLUDE_TESTS=OFF"
   --cmake-opt="-DSYCL_INCLUDE_TESTS=OFF"
   --cmake-opt="-DNATIVECPU_USE_OCK=OFF"
+  --llvm-external-projects="lld"
+)
+python "${args[@]}"
+
+python buildbot/compile.py
+
+###############################################################################
+
+# just opencl, no native_cpu, no level_zero
+
+args=(
+  buildbot/configure.py
+  --use-lld
+  --host-target="host;RISCV;AArch64"
+  --cmake-opt="-DFETCHCONTENT_BASE_DIR=$PWD/_demos/_deps_linux"
+  --cmake-opt="-DLLVM_INCLUDE_TESTS=OFF"
+  --cmake-opt="-DSYCL_INCLUDE_TESTS=OFF"
+  --cmake-opt="-DNATIVECPU_USE_OCK=OFF"
+  --cmake-opt="-DSYCL_ENABLE_BACKENDS=\"opencl\""
   --llvm-external-projects="lld"
 )
 python "${args[@]}"

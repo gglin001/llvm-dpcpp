622c2a6441e8126d7fd110b9fb24fcc8fc20c9d6
Allen Guo <guosonglin001@gmail.com>
2024-09-14 22:25:02 +0800

    wip

--- _demos/t0.build.sh
+++ _demos/t0.build.sh
@@ -6,6 +6,7 @@ cmake --build build --target all
 ###############################################################################
 
 # OSX
+# NATIVECPU_USE_OCK=ON
 
 args=(
   buildbot/configure.py
@@ -15,12 +16,16 @@ args=(
   --cmake-opt="-DFETCHCONTENT_BASE_DIR=$PWD/_demos/_deps_osx"
   --cmake-opt="-DLLVM_INCLUDE_TESTS=OFF"
   --cmake-opt="-DSYCL_INCLUDE_TESTS=OFF"
+  --cmake-opt="-DNATIVECPU_USE_OCK=ON"
 )
 python "${args[@]}"
 
 python buildbot/compile.py -t sycl-toolchain
 
 ###############################################################################
+
+# OSX
+# just opencl, no native_cpu, no level_zero
 
 args=(
   buildbot/configure.py
@@ -30,7 +35,7 @@ args=(
   --cmake-opt="-DLLVM_INCLUDE_TESTS=OFF"
   --cmake-opt="-DSYCL_INCLUDE_TESTS=OFF"
   --cmake-opt="-DNATIVECPU_USE_OCK=OFF"
-  --cmake-opt="-DSYCL_ENABLE_BACKENDS=\"opencl\""
+  --cmake-opt="-DSYCL_ENABLE_BACKENDS=opencl"
 )
 python "${args[@]}"
 
@@ -85,7 +90,7 @@ args=(
   --cmake-opt="-DLLVM_INCLUDE_TESTS=OFF"
   --cmake-opt="-DSYCL_INCLUDE_TESTS=OFF"
   --cmake-opt="-DNATIVECPU_USE_OCK=OFF"
-  --cmake-opt="-DSYCL_ENABLE_BACKENDS=\"opencl\""
+  --cmake-opt="-DSYCL_ENABLE_BACKENDS=opencl"
   --llvm-external-projects="lld"
 )
 python "${args[@]}"

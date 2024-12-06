a6639969c75cf32fcc6282a767e86842d5423c14
Allen Guo <guosonglin001@gmail.com>
2024-12-06 18:45:21 +0800

    wip

--- _demos/t5.build-cross.sh
+++ _demos/t5.build-cross.sh
@@ -5,7 +5,7 @@ cmake --preset osx_allen -S$PWD/llvm -B$PWD/build
 cmake --build $PWD/build --target all
 # cmake --build $PWD/build --target distribution
 
-cmake --build $PWD/build --target help >_demos/cmake.build.log
+cmake --build $PWD/build --target help >_demos/cmake.target.help.log
 
 ###############################################################################
 
@@ -14,7 +14,6 @@ cmake --preset cross_allen -S$PWD/llvm -B$PWD/build-cross
 # cmake --build $PWD/build-cross --target all
 cmake --build $PWD/build-cross --target libsycldevice
 cmake --build $PWD/build-cross --target libsycl.so
-# cmake --build $PWD/build-cross --target all
 
 cmake --build $PWD/build-cross --target help >_demos/cmake.build-cross.log
 

6945b1554aac7095ff9872597f38b0f09ec0c6c8
Allen Guo <guosonglin001@gmail.com>
2024-12-25 22:20:44 +0800

    1/N add SYCL_ENABLE_COMPILER

---
+++ _demos/t2.build_sycl.sh
@@ -0,0 +1,31 @@
+###############################################################################
+
+ln -s $PWD/sycl/CMakePresets.json $PWD/llvm/CMakePresets.json
+
+###############################################################################
+
+cmake --preset osx -S$PWD/llvm -B$PWD/build
+
+cmake --build $PWD/build --target all
+# cmake --build $PWD/build --target distribution
+
+cmake --build $PWD/build --target help >_demos/cmake.target.help.log
+
+###############################################################################
+
+cmake --preset sycl -S$PWD/llvm -B$PWD/build-sycl
+
+# cmake --build $PWD/build-sycl --target all
+cmake --build $PWD/build-sycl --target libsycldevice
+cmake --build $PWD/build-sycl --target libsycl.so
+
+cmake --build $PWD/build-sycl --target help >_demos/cmake.sycl.target.help.log
+
+###############################################################################
+
+# TODO: can we use `-sycl-device-library-location` in CMD ?
+
+# find build-sycl/lib -name "libsycl*.o" -printf "%f\n" | xargs -d"\n" -I{} echo {}
+find build-sycl/lib -name "libsycl*.o" -printf "%f\n" | xargs -d"\n" -I{} ln -s $PWD/build-sycl/lib/{} $PWD/build/lib/
+
+###############################################################################
